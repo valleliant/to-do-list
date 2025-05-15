e68f29b36bfdd1bf8edc649c7eefed8f214c6da1
valleliant <thibaud.vallelian@studentfr.ch>
2025-05-15 16:40:07 +0200

    v0.08 - fix des bugs

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
+/// <reference types="next/image-types/global" />
 
-declare namespace React {
-  interface JSX {}
-} 
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
